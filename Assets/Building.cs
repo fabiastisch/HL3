1c17762b7fbb53f2548f8693df22850818f9060f
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-04-04 02:42:33 +0200

    WIP - fixing bugs

--- Assets/Building.cs
+++ Assets/Building.cs
@@ -6,17 +6,46 @@ using UnityEngine;
 
 public class Building : MonoBehaviour {
     public LayerMask buildableMask;
+    public GameObject collisionBox;
     private Vector3 center;
     private static List<Building> currentlyChecked = new List<Building>();
 
+    private float shpereRadius = 2f;
+    private Vector3 halfExtents = new Vector3(1.5f,1f,1.5f);
+    
+    
+    public float ScaleX = 2.0f;
+    public float ScaleY = 2.0f;
+    public float ScaleZ = 2.0f;
+    public bool RecalculateNormals = false;
+    private Vector3[] _baseVertices;
     // Start is called before the first frame update
     void Start() {
         center = GetComponent<Renderer>().bounds.center;
     }
 
     private void OnDrawGizmos() {
-        /*Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(GetComponent<Renderer>().bounds.center, 2.3f);*/
+        Gizmos.color = Color.yellow;
+        // Mesh mesh = GetComponent<MeshCollider>().sharedMesh;
+        Mesh mesh = Utils.CopyMesh(GetComponent<MeshFilter>().mesh);
+        Debug.Log(mesh);
+        if (_baseVertices == null)
+            _baseVertices = mesh.vertices;
+        var vertices = new Vector3[_baseVertices.Length];
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            var vertex = _baseVertices[i];
+            vertex.x = vertex.x * ScaleX;
+            vertex.y = vertex.y * ScaleY;
+            vertex.z = vertex.z * ScaleZ;
+            vertices[i] = vertex;
+        }
+        mesh.vertices = vertices;
+        if (RecalculateNormals)
+            mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        // Gizmos.DrawSphere(GetComponent<Renderer>().bounds.center, shpereRadius);
+        Gizmos.DrawMesh(mesh, center);
     }
 
     /**
@@ -26,7 +55,7 @@ public class Building : MonoBehaviour {
         Debug.Log("OnDestory");
         // Say every neighbour that this object will be destoryed.
         // Debug.Log("Center: " + center);
-        Collider[] colliders = Physics.OverlapSphere(center, 2.3f, buildableMask);
+        Collider[] colliders = GetCollider();
         Debug.Log("Initial Found Neighbours: " + colliders.Length);
         foreach (var neighbour in colliders) {
             // check for every neighbour if is grounded
@@ -45,7 +74,7 @@ public class Building : MonoBehaviour {
         Vector3 pos = o.gameObject.GetComponent<Building>().GetCenter();
         // Debug.Log("Center: " + pos + "\n on " + GetComponent<Renderer>().bounds.center );
         Debug.DrawLine(pos, GetComponent<Renderer>().bounds.center, Color.yellow, float.MaxValue);
-        Collider[] colliders = Physics.OverlapSphere(center, 2.3f, buildableMask);
+        Collider[] colliders = GetCollider();
         bool grounded = false;
         List<Building> buildings = new List<Building>();
         foreach (var neighbour in colliders) {
@@ -76,8 +105,14 @@ public class Building : MonoBehaviour {
         
     }
 
+    private Collider[] GetCollider() {
+        Physics.OverlapBox(center, halfExtents, Quaternion.identity);
+        
+        return Physics.OverlapSphere(center, shpereRadius, buildableMask);
+    }
+
     private bool FindGround(GameObject o) {
-        Collider[] colliders = Physics.OverlapSphere(center, 2.3f, buildableMask);
+        Collider[] colliders = GetCollider();
         bool grounded = false;
         List<Building> buildings = new List<Building>();
         foreach (var neighbour in colliders) {
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -4,4 +4,15 @@ public class Utils {
     public static float getDistanceBetweenGameObjects(GameObject g1, GameObject g2) {
         return Vector3.Magnitude(g1.transform.position - g2.transform.position);
     }
+
+    public static Mesh CopyMesh(Mesh mesh) {
+        Mesh newMesh = new Mesh();
+        newMesh.vertices = mesh.vertices;
+        newMesh.triangles = mesh.triangles;
+        newMesh.uv = mesh.uv;
+        newMesh.normals = mesh.normals;
+        newMesh.colors = mesh.colors;
+        newMesh.tangents = mesh.tangents;
+        return newMesh;
+    }
 }
